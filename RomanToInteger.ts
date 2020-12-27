7afcfc15494863767a1149930b5266650bf490c6
ydaydada@gmail.com <rekuill@hotmail.com>
2020-12-27 11:09:39 +0900

    13. RomanToInteger

---
+++ RomanToInteger.ts
@@ -0,0 +1,36 @@
+/**
+ * leetcode problem 13.Roman to Integer
+ * https://leetcode.com/problems/roman-to-integer/
+ *
+ * ts-nodeで実行
+ * ex: npm run ts-node RomanToInteger.ts.ts III
+ * result [1,2]
+ *
+ * [todo]
+ * - jestでテスト書く
+ */
+
+const s: string = process.argv[2];
+
+function romanToInt(s: string): number {
+  const hashTable = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  } as const;
+
+  return s
+    .split('')
+    .map((cur, i) => {
+      return hashTable[cur] > hashTable[s[i - 1]]
+        ? hashTable[cur] - hashTable[s[i - 1]] * 2
+        : hashTable[cur];
+    })
+    .reduce((acc, cur) => acc + cur);
+}
+
+romanToInt(s);
